0c4f2774578a508892777f0443efb97097908192
SpencerBrown <spencer@spencerbrown.org>
2017-03-27 01:03:58 -0500

    Implement better parsing of config file, still just creating root certificate

--- mongodb-x509.go
+++ mongodb-x509.go
@@ -12,39 +12,71 @@ import (
 	"math/big"
 	"log"
 	"crypto/x509/pkix"
+	"path"
 )
+
+type m_config struct {
+	o         []string
+	ou        []string
+	algorithm algor
+	keylength int
+	expiry    time.Duration
+}
+
+type algor int
+const (
+	RSA algor = iota
+	ECDSA
+)
+
+type m_config_rules struct {
+	key  string
+	def  interface{}
+}
 
 func main() {
 
-	config, err := toml.LoadFile("mongodb-x509.toml")
+	toml_config, err := toml.LoadFile("mongodb-x509.toml")
 	if err != nil {
-		fmt.Println("Error ", err.Error())
-		return
+		log.Fatalf("Failed to parse config file: %v", err)
 	}
 
-	rootconfig := config.Get("root").(*toml.TomlTree)
-	rootOraw := rootconfig.Get("O").(string)
-	rootO := make([]string, 1)
-	rootO[0] = rootOraw
-	rootOUraw := rootconfig.Get("OU").(string)
-	rootOU := make([]string, 1)
-	rootOU[0] = rootOUraw
-	rootalgorithm := rootconfig.Get("algorithm").(string)
-	rootkeylength := rootconfig.Get("keylength").(int64)
-	rootexpiryraw := rootconfig.Get("expiry").(int64)
-	rootexpiry := time.Duration(rootexpiryraw) * time.Hour
+	// root CA certificate configuration and defaults
+	root_config_rules := []m_config_rules{
+		{"O", "MongoDB" },
+		{"OU", "Certificate Authority" },
+		{"algorithm", RSA },
+		{"keylength", 2048 },
+		{"expiry", 365 },
+	}
 
-	fmt.Println(rootO, rootOU, rootalgorithm, rootkeylength, rootexpiry)
+	root_tree := toml_config.Get("root")
+	if root_tree == nil {
+		log.Fatal("Config file must have [root] section")
+	}
+	switch root_tree.(type) {
+	case *toml.TomlTree:
+	default:
+		log.Fatal("Config file must have [root] section")
+
+	}
+	root_config, err := get_m_config(root_tree.(*toml.TomlTree), root_config_rules)
+	if err != nil {
+		log.Fatalf("Invalid config file: %v", err)
+	}
+
+
+	fmt.Println(root_config)
 
 	// Generate RSA private key
-	priv, err := rsa.GenerateKey(rand.Reader, int(rootkeylength))
+	priv, err := rsa.GenerateKey(rand.Reader, root_config.keylength)
 	if err != nil {
 		log.Fatalf("Failed to generate private key: %s", err)
 	}
 
 	// Generate self-signed root certificate
 	notBefore := time.Now()
-	notAfter := notBefore.Add(rootexpiry)
+	notAfter := notBefore.Add(root_config.expiry)
 
 	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
 	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
@@ -55,8 +87,8 @@ func main() {
 	root_template := x509.Certificate{
 		SerialNumber: serialNumber,
 		Subject: pkix.Name{
-			Organization:       rootO,
-			OrganizationalUnit: rootOU,
+			Organization:       root_config.o,
+			OrganizationalUnit: root_config.ou,
 		},
 		NotBefore:             notBefore,
 		NotAfter:              notAfter,
@@ -70,7 +102,14 @@ func main() {
 	}
 
 	// Write root CA certificate
-	certOut, err := os.Create("root-ca.pem")
+	root_path := path.Join("tls-root-ca")
+	root_path_private := path.Join(root_path, "private")
+	err = os.MkdirAll(root_path_private, 0755)
+	if err != nil {
+		log.Fatalf("Failed to create directory for root CA certificate: %s", err)
+	}
+	root_ca_path := path.Join(root_path, "root-ca.pem")
+	certOut, err := os.Create(root_ca_path)
 	if err != nil {
 		log.Fatalf("Failed to open root certificate file for writing: %s", err)
 	}
@@ -80,7 +119,8 @@ func main() {
 	log.Print("Written root certificate")
 
 	// Write private key for root CA certificate
-	keyOut, err := os.OpenFile("root-ca-key.pem", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	root_key_path := path.Join(root_path_private, "root-ca-key.pem")
+	keyOut, err := os.OpenFile(root_key_path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
 		log.Fatalf("Failed to open key file for writing: %s", err)
 	}
@@ -91,12 +131,85 @@ func main() {
 
 }
 
-/*
-[root]
-O = "MongoDB"
-OU = "Certificate Authority"
-algorithm = "RSA"
-keylength = 2048
-# expiration in days
-expiry = 1000
- */
+func get_m_config(tree *toml.TomlTree, rules []m_config_rules) (*m_config, error) {
+	var cf m_config
+	tree_map := tree.ToMap()
+
+	// fill in default values
+	for _, rule := range rules {
+		switch rule.key {
+		case "O":
+			cf.o = append(cf.o, rule.def.(string))
+		case "OU":
+			cf.ou = append(cf.ou, rule.def.(string))
+		case "algorithm":
+			cf.algorithm = rule.def.(algor)
+		case "keylength":
+			cf.keylength = rule.def.(int)
+		case "expiry":
+			cf.expiry = time.Duration(rule.def.(int)) * time.Hour
+		}
+	}
+
+	// fill in user specified values
+	for item, value := range tree_map {
+		found_item := false
+		for _, rule := range rules {
+			if rule.key == item {
+				found_item = true
+				switch item {
+				case "O":
+					switch value.(type) {
+					case string:
+						 cf.o = append(cf.o, value.(string))
+					default:
+						return nil, fmt.Errorf("Key %q should have a string value", item)
+					}
+				case "OU":
+					switch value.(type) {
+					case string:
+						cf.ou = append(cf.ou, value.(string))
+					default:
+						return nil, fmt.Errorf("Key %q should have a string value", item)
+					}
+				case "algorithm":
+					switch value.(type) {
+					case string:
+						switch value.(string) {
+						case "RSA":
+							cf.algorithm = RSA
+						case "ECDSA":
+							cf.algorithm = ECDSA
+						default:
+							return nil, fmt.Errorf("algorithm must be \"RSA\" or \"ECDSA\", was %q", value.(string))
+						}
+					default:
+						return nil, fmt.Errorf("Key %q should have a string value", item)
+					}
+				case "keylength":
+					switch value.(type) {
+					case int64:
+						//TODO check for valid keylength values
+						cf.keylength = int(value.(int64))
+					default:
+						return nil, fmt.Errorf("Key %q should have a numeric value", item)
+					}
+				case "expiry":
+					switch value.(type) {
+					case int64:
+						//TODO check for valid expiry values
+						cf.expiry = time.Duration(value.(int64)) * time.Hour
+					default:
+						return nil, fmt.Errorf("Key %q should have a numeric value", item)
+					}
+				default:
+					log.Fatalf("Something wrong in m_config_rules! %q", item)
+				}
+			}
+		}
+		if !found_item {
+			return nil, fmt.Errorf("Unknown key %q in config file", item)
+		}
+	}
+	return &cf, nil
+}
